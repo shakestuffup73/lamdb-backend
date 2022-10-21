813fabcc0d2070a204ec0dbab86e4938041fd53d
mstevenson <mbstevenson96@gmail.com>
2022-10-21 15:25:10 -0400

    create pet routes and controllers

---
+++ routes/pets.js
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+import * as petsCtrl from '../controllers/pets.js'
+
+const router = Router()
+
+// ========== Public Routes ===========
+
+
+// ========= Protected Routes ========= 
+router.use(decodeUserFromToken)
+router.get('/', checkAuth, petsCtrl.index)
+router.post('/', checkAuth, petsCtrl.create)
+router.get('/:id', checkAuth, petsCtrl.show)
+router.put('/:id', checkAuth, petsCtrl.update)
+router.delete('/:id', checkAuth, petsCtrl.delete)
+
+
+
+export { router }
