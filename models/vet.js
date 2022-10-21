29fb46ada433f1d7229136627c5a3de1970d095c
mstevenson <mbstevenson96@gmail.com>
2022-10-21 14:34:04 -0400

    create vet model

---
+++ models/vet.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const vetSchema = new Schema({
+  name: String,
+  contact: String,
+  description: String,
+  diagnosis: String,
+  results: String,
+  medications: String,
+  weight: Number,
+  cost: Number,
+  appointment: String,
+  rabies: String,
+},{
+  timestamps: true,
+})
+
+const Vet = mongoose.model('Vet', vetSchema)
+
+export { Vet }
