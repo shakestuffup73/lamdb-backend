c04c34bda5c3ce5718fbdbb84536cf3e82fbf390
Amanda Stern <amanda.stern.73@gmail.com>
2022-10-21 15:37:31 -0400

    write controller function for photoUpload, install Cloudinary

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,7 +1,26 @@
 import { Pet } from "../models/pet"
+import { v2 as cloudinary } from 'cloudinary'
 
-
+function addPhoto(req, res) {
+  const imageFile = req.files.photo.path
+  Pet.findById(req.params.id)
+  .then(pet => {
+    cloudinary.uploader.upload(imageFile, {tags: `${req.user.email}`})
+    // In an application with auth you'll use: {tags: `${req.user.email}`}
+    .then(image => {
+      pet.photo = image.url
+      pet.save()
+      .then(pet => {
+        res.status(201).json(pet.photo)
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+  })
+}
 
 export {
-
+  addPhoto
 }
