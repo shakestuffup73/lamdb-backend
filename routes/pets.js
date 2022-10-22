33c89b1961d80e36c514cda5a06e9d987919afdf
Amanda Stern <amanda.stern.73@gmail.com>
2022-10-22 14:50:56 -0400

    write show controller function

--- routes/pets.js
+++ routes/pets.js
@@ -11,7 +11,7 @@ const router = Router()
 router.use(decodeUserFromToken)
 router.get('/', checkAuth, petsCtrl.index)
 router.post('/', checkAuth, petsCtrl.create)
-// router.get('/:id', checkAuth, petsCtrl.show)
+router.get('/:id', checkAuth, petsCtrl.show)
 // router.put('/:id', checkAuth, petsCtrl.update)
 // router.delete('/:id', checkAuth, petsCtrl.delete)
 
