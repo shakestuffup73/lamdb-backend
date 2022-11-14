cd9910235f828aeb0918c21249c3920e10f04e9f
Amanda Stern <amanda.stern.73@gmail.com>
2022-11-14 14:36:27 -0500

    deploy

--- middleware/auth.js
+++ middleware/auth.js
@@ -19,12 +19,6 @@ const decodeUserFromToken = (req, res, next) => {
   }
 }
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 function checkAuth(req, res, next) {
   return req.user ? next() : res.status(401).json({ msg: 'Not Authorized' })
 }
